708b192b8d10ed64677df966d04175578a2085c7
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-03-27 14:11:48 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,10 +13,12 @@ def happy_birthday(birthday_kids)
 end
 
 
-def happy_birthday(birthday_kids)
+def age_appropriate_birthday(birthday_kids)
   
   birthday_kids.each {|name, age| 
-    puts "Happy Birthday #{name}! You are now #{age} years old!"
+    if age <= 12  
+      puts "Happy Birthday #{name}! You are now #{age} years old!"
+    end
   }
   
 end
