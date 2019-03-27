3ded6373801310877ee1111d3126c533734d6662
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-03-27 14:10:50 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,3 +13,10 @@ def happy_birthday(birthday_kids)
 end
 
 
+def happy_birthday(birthday_kids)
+  
+  birthday_kids.each {|name, age| 
+    puts "Happy Birthday #{name}! You are now #{age} years old!"
+  }
+  
+end
